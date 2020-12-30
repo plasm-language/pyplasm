597b0cca25d89f184eececf61df83d8d72fb2966
Giorgio Scorzelli <scrgiorgio@gmail.com>
2020-12-30 13:45:28 +0100

    Trying to fix bigsur

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -5,10 +5,12 @@ BUILD_DIR=${BUILD_DIR:-$(pwd)/build}
 Python_EXECUTABLE=$(which python${PYTHON_VERSION})
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR} 
+
 cmake -DPython_EXECUTABLE=${Python_EXECUTABLE} ../
 cmake --build ./ --target all     --config Release --parallel 4
 cmake --build ./ --target install --config Release
 
-export PYTHONPATH=build/Release
+cd Release/pyplasm
+export PYTHONPATH=../
 ${Python_EXECUTABLE} -c "from pyplasm import *"
 
