01c9cce4e3092a1760297ab1d50fb23ae112c687
Giorgio Scorzelli <scrgiorgio@gmail.com>
2020-12-30 10:10:21 +0100

    Trying to fix bigsur

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -9,3 +9,6 @@ cmake -DPython_EXECUTABLE=${Python_EXECUTABLE} ../
 cmake --build ./ --target all     --config Release --parallel 4
 cmake --build ./ --target install --config Release
 
+# test it
+${Python_EXECUTABLE} -c "from pyplasm import *" 
+
