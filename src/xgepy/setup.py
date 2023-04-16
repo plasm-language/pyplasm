f133940e6cdfde91d2cdfe869cbe40f5e4570bad
Giorgio Scorzelli <scrgiorgiO@gmail.com>
2023-04-16 14:27:16 -0600

    fixing conda

--- src/xgepy/setup.py
+++ src/xgepy/setup.py
@@ -19,37 +19,24 @@ def findFilesInCurrentDirectory():
 			ret.append(file)
 	return ret
 
+from setuptools import setup
 is_conda = os.path.exists(os.path.join(sys.prefix, 'conda-meta', 'history'))
 if is_conda:
-	from setuptools import setup
 	import conda_build.bdist_conda
-	setup(
-		name = "pyplasm",
-		description = "Plasm is a design language for geometric and solid parametric design, developed by the CAD Group at the Universities La Sapienza and Roma Tre in Italy",
-		version=VERSION,
-		url="https://github.com/plasm-language/pyplasm",
-		author="Giorgio Scorzelli",
-		author_email="scorzell@dia.uniroma3.it",
-		packages=["pyplasm"],
-		package_dir={"pyplasm":'.'},
-		package_data={"pyplasm": findFilesInCurrentDirectory()},
-		platforms=['Linux', 'OS-X', 'Windows'],
-		license = "GPL",
-		distclass=conda_build.bdist_conda.CondaDistribution,
-		conda_buildnum=1)
-	
+	extra_args={"distclass": conda_build.bdist_conda.CondaDistribution,"conda_buildnum": 1}	
 else:
-	from setuptools import setup
-	import conda_build.bdist_conda	
-	setuptools.setup(
-		name = "pyplasm",
-		description = "Plasm is a design language for geometric and solid parametric design, developed by the CAD Group at the Universities La Sapienza and Roma Tre in Italy",
-		version=VERSION,
-		url="https://github.com/plasm-language/pyplasm",
-		author="Giorgio Scorzelli",
-		author_email="scorzell@dia.uniroma3.it",
-		packages=["pyplasm"],
-		package_dir={"pyplasm":'.'},
-		package_data={"pyplasm": findFilesInCurrentDirectory()},
-		platforms=['Linux', 'OS-X', 'Windows'],
-		license = "GPL")
+	extra_args={}
+
+setup(
+	name = "pyplasm",
+	description = "Plasm is a design language for geometric and solid parametric design, developed by the CAD Group at the Universities La Sapienza and Roma Tre in Italy",
+	version=VERSION,
+	url="https://github.com/plasm-language/pyplasm",
+	author="Giorgio Scorzelli",
+	author_email="scorzell@dia.uniroma3.it",
+	packages=["pyplasm"],
+	package_dir={"pyplasm":'.'},
+	package_data={"pyplasm": findFilesInCurrentDirectory()},
+	platforms=['Linux', 'OS-X', 'Windows'],
+	license = "GPL",
+	**extra_args)
