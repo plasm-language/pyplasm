fd706e8c88617d05ccfecb271a3b137d35cf9412
Giorgio Scorzelli <scrgiorgiO@gmail.com>
2023-04-16 18:34:47 -0600

    fixing conda

---
+++ scripts/conda.ubuntu.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -ex
+
+# avoid conflicts with pip packages installed using --user
+export PYTHONNOUSERSITE=True  
+
+# install conda
+pushd ~
+curl -L -O https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+bash Miniforge3-Linux-x86_64.sh -b 
+rm -f Miniforge3-Linux-x86_64.sh
+popd
+
+# activate conda
+source ~/miniforge3/etc/profile.d/conda.sh || true # can be already activated
+conda config --set always_yes yes --set anaconda_upload no
+conda create --name my-env -c conda-forge python=${PYTHON_VERSION} numpy conda anaconda-client conda-build wheel pyopengl freeglut 
+conda activate my-env
+
+PYTHON=`which python`
+
+# compile pyplas
+mkdir -p build
+cd build
+cmake -DPython_EXECUTABLE=${PYTHON} ../
+make -j
+make install
+
+# distrib
+pushd Release/pyplasm 
+rm -Rf $(find ${CONDA_PREFIX} -iname "pyplasm*.tar.bz2") || true
+$PYTHON setup.py -q bdist_conda
+CONDA_FILENAME=$(find ${CONDA_PREFIX} -iname "pyplasm*.tar.bz2" | head -n 1)
+GIT_TAG=`git describe --tags --exact-match 2>/dev/null || true`
+if [[ "${GIT_TAG}" != "" ]] ; then
+  anaconda --verbose --show-traceback -t ${ANACONDA_TOKEN} upload ${CONDA_FILENAME} --no-progress 
+fi
+popd
+
+
+
+
+
---
+++ scripts/cpython.ubuntu.sh
@@ -0,0 +1,32 @@
+
+
+#!/bin/bash
+
+set -ex
+
+PYTHON=`which python${PYTHON_VERSION}`
+
+# this is for linux/docker
+yum install -y libffi-devel
+
+# make sure pip is updated
+${PYTHON} -m pip install --upgrade pip || true
+
+# compile
+mkdir -p build
+cd build
+cmake -DPython_EXECUTABLE=${PYTHON} ../
+make -j
+make install
+
+# distrib
+cd Release/pyplasm
+rm -Rf ./dist
+$PYTHON -m pip install --upgrade pip || true 
+$PYTHON -m pip install setuptools wheel cryptography==3.4.0 twine || true
+PYTHON_TAG=cp$(echo $PYTHON_VERSION | awk -F'.' '{print $1 $2}')
+$PYTHON setup.py -q bdist_wheel --python-tag=$PYTHON_TAG --plat-name=manylinux2010_x86_64
+GIT_TAG=`git describe --tags --exact-match 2>/dev/null || true`
+if [[ "${GIT_TAG}" != "" ]] ; then
+  $PYTHON -m twine upload --username ${PYPI_USERNAME} --password ${PYPI_TOKEN} --skip-existing   "dist/*.whl" 
+fi
